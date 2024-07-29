397e8ffa08ac981b7f61391a2470519890fc0344
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2024-07-29 06:58:25 +0000

    container config done

---
+++ Python/lecture3.py
@@ -0,0 +1,22 @@
+# ---------List & Tuple in Python --------------->
+marks = [94.4, 87.5, 95.2, 66.4, 45.1]
+print(marks)
+print(type(marks))
+print(len(marks))
+
+# --------------List Slicing------------------>
+
+list = [85, 94, 76, 63, 48]
+print(list[1:4])
+
+# -------------List Methods------------------->
+
+list = [2, 1, 3]
+list.append(4)
+list.sort()
+list.sort( reverse = True)
+list.reverse()
+list.insert(3,4)
+list.remove(1)
+list.pop(0)
+print(list) 
