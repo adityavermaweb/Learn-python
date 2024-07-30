0bf461eb736f8fecec45859a2fa85f50480b4352
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2024-07-30 07:09:19 +0000

    container config done

--- Python/lecture4.py
+++ Python/lecture4.py
@@ -19,3 +19,40 @@ student = {
     }
 }
 print(student["score"]["python"])
+
+# ----------------- Dictionary Methods --------------->
+
+student = {
+    "name" : "Aditya",
+    "score" : {
+        "python" : 92,
+        "javaScript" : 89,
+        "css" : 95
+    }
+}
+
+print(student.keys())
+print(student.values())
+print(student.items())
+print(student.get("name"))
+
+new_dict = {"name" : "Ayush", "age" : 20}
+student.update(new_dict)
+print(student)
+
+
+
+
+# ----------------Set in Python ---------------->
+
+collection = {1, 2, 3, 4, "hello", "world"}
+
+print(collection)
+print(type(collection))
+
+
+# Create a empty set syntax -->
+
+collection = set() #empty set: syntax
+
+print(type(collection))
