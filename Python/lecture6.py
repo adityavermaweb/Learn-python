75308579758df1c75e83422ac901d2fb64ca7024
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2024-08-05 17:57:18 +0000

    container config done

--- Python/lecture6.py
+++ Python/lecture6.py
@@ -1,24 +1,34 @@
 # --------------------Function & Recursion---------------------->
 # Eg 1: Write a function to print Hello ?
 
-# def wish ():
-#     print("Hello Good Morning")
-# wish() 
-# wish()   
+def wish ():
+    print("Hello Good Morning")
+wish() 
+wish()   
 
 
 # Eg 2: Write a function to take name of the student as input and print wish messages by name.
 
-# def wish(name):
-#     print("Hello",name,"Good Morning")
-# wish("Aditya")
-# wish("Durga")  
+def wish(name):
+    print("Hello",name,"Good Morning")
+wish("Aditya")
+wish("Durga")  
 
 
 # Eg 3: Write a function to take number as input and print its square value.
 
-# def squareIt(number):
-#     print("The Square of",number,"is",number*number)
-# squareIt(4)
-# squareIt(5)
-# squareIt(2)    
+def squareIt(number):
+    print("The Square of",number,"is",number*number)
+squareIt(4)
+squareIt(5)
+squareIt(2)    
+
+# Eg 4: Write a function of calculate average of 3 numbers
+
+def calc_avg(a, b, c):
+    sum = a + b + c
+    avg = sum / 3
+    print(avg)
+    return avg
+
+calc_avg(98, 97, 95)
