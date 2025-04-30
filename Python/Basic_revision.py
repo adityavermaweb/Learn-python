e2f34911154d972ded3b1c549158286a5becb736
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-04-30 16:14:54 +0000

    container config done

--- Python/Basic_revision.py
+++ Python/Basic_revision.py
@@ -12,3 +12,16 @@
 
 # is_genius = True
 # print(is_genius)
+
+
+# name = input("What is your name :")
+# print(name)
+
+# age = 19
+# if age >= 18:
+#     print("You are an adult")
+#     print("You can vote")
+# elif age < 18 and age > 3 :
+#     print ("You are in school")
+# else:
+#     print("You are in school")
