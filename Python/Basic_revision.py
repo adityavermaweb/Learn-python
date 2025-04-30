aa98a880aa7021ec669640baf157c7f1ef232c03
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-04-30 16:14:14 +0000

    container config done

---
+++ Python/Basic_revision.py
@@ -0,0 +1,14 @@
+# print("Hello Aditya")
+# name = "Aditya"
+# age = 23
+# print(name, age)
+
+# is_adult = True
+
+# fname = "Tony"
+# lname = "Stark"
+# age = 51
+# print (fname, lname, age)
+
+# is_genius = True
+# print(is_genius)
